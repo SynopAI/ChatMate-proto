7230a5713a4d3c84d22b868bd1845537c850bf56
Snychng <snychng@gmail.com>
2024-05-19 08:01:32 +0800

    更新脚本

--- open_anything.py
+++ open_anything.py
@@ -1,7 +1,4 @@
 import subprocess
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import time
 
 # # 打开电脑上的应用程序（例如文本编辑器）
 # def open_application():
@@ -19,33 +16,6 @@ def open_application(app_path):
     except Exception as e:
         print(f"Error opening application: {e}")
 
-# 控制浏览器并进行搜索
-def search_in_browser(query):
-    try:
-        # 初始化Chrome浏览器
-        driver = webdriver.Chrome()  # 确保chromedriver在你的PATH中
-        driver.get("http://www.google.com")
-
-        # 等待页面加载
-        time.sleep(2)
-
-        # 找到搜索框并输入查询
-        search_box = driver.find_element_by_name("q")
-        search_box.send_keys(query)
-        search_box.send_keys(Keys.RETURN)
-
-        # 等待搜索结果加载
-        time.sleep(5)
-        
-        # 关闭浏览器
-        driver.quit()
-    except Exception as e:
-        print(f"Error controlling browser: {e}")
-
 if __name__ == "__main__":
     # 打开文本编辑器
-    open_application('/Applications/Lark.app/Contents/MacOS/Feishu')
-    
-    # # 在浏览器中搜索特定文本
-    # search_query = "Python programming"
-    # search_in_browser(search_query)
+    open_application('/Applications/[APPNAME].app/Contents/MacOS/[APPNAME]')
