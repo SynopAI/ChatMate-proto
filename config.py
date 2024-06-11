8ce5c0799a534ccd33723729b13deb147f807bfb
Snychng <snychng@gmail.com>
2024-06-11 10:11:00 +0800

    更新 system_prompt

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ class Config:
     MODEL_NAME = "gpt-4o"
     TTS_MODEL = "tts-1-1106"
     TTS_VOICE = "echo"
-    SYSTEM_PROMPT = "You are a helpful assistant"
+    SYSTEM_PROMPT = "你是一个人工智能助手，我会给你一张我当前电脑屏幕的截图，并且我会问你一些问题，有可能我的问题与截图无关，你需要帮我解决我的问题，一般来说请使用简体中文回答我的问题"
     HISTORY = []
     MAX_HISTORY_LENGTH = 10
 
